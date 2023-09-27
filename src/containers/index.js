8562912c44ba4ea7bb7e696ba284c401ec2e6b27
Matthew <matthewajustice@gmail.com>
2023-09-27 00:26:17 -0400

    attempting build fix 2

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Landing } from '@/containers/Landing.astro';
-export { default as About } from '@/containers/About.astro';
-export { default as Examples } from '@/containers/Examples.jsx';
-export { default as Services } from '@/containers/Services.astro';
