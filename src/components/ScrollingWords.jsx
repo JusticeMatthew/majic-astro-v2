925f7c2e148575baef8fd68645a77cb200f6e12d
Matthew <matthewajustice@gmail.com>
2024-04-04 22:13:43 -0400

    Finally found build error culprit, for some reason something in the services broke so ill need to fix that

--- src/components/ScrollingWords.jsx
+++ src/components/ScrollingWords.jsx
@@ -1,34 +1,14 @@
-import ParallaxText from './ParallaxText';
+import ParallaxText from './ParallaxText.jsx';
 
 export default function ScrollingWords() {
   return (
     <div className="flex flex-row min-h-[16rem] sm:min-h-[24rem] pointer-events-none items-center">
-      {/* <p
-        transition={{
-          duration: 2,
-        }}
-        className="flex items-center overflow-visible uppercase text-7xl text-gradient font-cursive sm:text-9xl mg:my-0"
-      > */}
       <ParallaxText
-        client:idle
         baseVelocity={1}
         className="uppercase text-7xl font-cursive sm:text-9xl md:my-0"
       >
         Abracadabra Presto Alakazam Hocus Pocus
       </ParallaxText>
-      {/* {['pocus', 'abracadabra', 'presto', 'alakazam', 'hocus'].map(
-          (item, idx) => (
-            <React.Fragment key={idx}>
-              <span>{item}</span>
-              <img
-                src="/images/logo-svg.svg"
-                alt=""
-                className="relative inline w-8 h-8"
-              />
-            </React.Fragment>
-          ),
-        )} */}
-      {/* </p> */}
     </div>
   );
 }
--- src/containers/LargeCTA.jsx
+++ src/containers/LargeCTA.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Button from '@/components/Button.jsx';
-import useMousePosition from '@/utils/useMousePosition';
 
 export default function LargeCTA() {
+  const useMousePosition = () => {
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    const updateMousePosition = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    useEffect(() => {
+      window.addEventListener('mousemove', updateMousePosition);
+      return () => {
+        window.removeEventListener('mousemove', updateMousePosition);
+      };
+    }, []);
+
+    return mousePosition;
+  };
+
   const { x, y } = useMousePosition();
 
   return (
