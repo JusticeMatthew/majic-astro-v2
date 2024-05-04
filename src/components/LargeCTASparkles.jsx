d5881d682a494030229c4a7c7c9f3c8f7eaab3c5
Matthew <matthewajustice@gmail.com>
2024-05-03 23:08:59 -0400

    fixed 3js sparkles

--- src/components/LargeCTASparkles.jsx
+++ src/components/LargeCTASparkles.jsx
@@ -5,15 +5,15 @@ export default function LargeCTASparkles() {
   return (
     <Canvas>
       <Sparkles
-        count={30}
-        scale={20}
+        count={20}
+        scale={10}
         size={15}
         speed={0.6}
         color={'#735CDD'}
         noise={5}
       />
       <Sparkles count={20} scale={20} size={10} />
-      <Sparkles count={20} scale={20} size={10} color={'#00A7E1'} />
+      <Sparkles count={20} scale={30} size={10} color={'#00A7E1'} />
     </Canvas>
   );
 }
