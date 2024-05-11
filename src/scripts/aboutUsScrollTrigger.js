581fa0d36fd4f891a88a849f7793cbccdb3dcd46
Matthew <matthewajustice@gmail.com>
2024-05-11 01:02:16 -0400

    adjusting gsap usage to reduce JS usage

--- src/scripts/aboutUsScrollTrigger.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from 'gsap';
-
-gsap.from('#content-about', {
-  opacity: 0,
-  duration: 1,
-  stagger: 0.3,
-  scrollTrigger: { trigger: '#content-about', start: 'bottom bottom' },
-});
